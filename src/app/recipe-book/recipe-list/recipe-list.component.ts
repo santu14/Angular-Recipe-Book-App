2c21cd2790357c0872df35e251062edc84d80a69
Santiago Solana <santiago.sjs@gmail.com>
2021-04-30 14:52:00 -0400

    added HTML templates for shopping list and recipe list

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model"
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,9 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
   constructor() { }
+  
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is a test', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'),
+    new Recipe('Test Recipe2', 'This is a test2', 'https://i2.wp.com/www.downshiftology.com/wp-content/uploads/2018/12/Shakshuka-19.jpg'),
 
+  ];
   ngOnInit(): void {
   }
 
